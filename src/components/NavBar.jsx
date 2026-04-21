12ef1be5a571a3cdb1e1de793bc0446e4fa956b0
Mahmoud-Al-Jaziri <119842984+Mahmoud-Al-Jaziri@users.noreply.github.com>
2026-04-22 05:56:12 +0800

    Added logout exit button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import { useNavigate, useLocation } from "react-router-dom";
+import { signOut } from 'firebase/auth'
+import { auth } from '../firebase'
 
 export default function NavBar(){
     const NAV_ITEMS = [
@@ -9,6 +11,15 @@ export default function NavBar(){
     ]
     const {pathname} = useLocation();
     const navigate = useNavigate();
+
+    const handleLogout = async () => {
+    try {
+      await signOut(auth)
+      navigate('/login')
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
     return(
         <Nav className="relay-nav justify-content-around py-2">
@@ -22,6 +33,19 @@ export default function NavBar(){
                     {label}
                 </Nav.Link>
             ))}
+            <Button
+                variant="link"
+                onClick={handleLogout}
+                className="logout-btn d-flex flex-column align-items-center p-0"
+                >
+                <span
+                    className="material-symbols-outlined logout-icon"
+                    
+                >
+                    logout
+                </span>
+                exit
+            </Button>
         </Nav>    
     )
 }
