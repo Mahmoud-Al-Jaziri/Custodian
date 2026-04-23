c8458d10414ef09709bb6cb8c147f3f903119978
Mahmoud-Al-Jaziri <119842984+Mahmoud-Al-Jaziri@users.noreply.github.com>
2026-04-23 20:44:25 +0800

    fixed Global.css import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles/global.css'
+import './styles/Global.css'
 import UserAuthProvider from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
