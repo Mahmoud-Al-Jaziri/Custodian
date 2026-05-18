bb839d0db2712e0c119dee9cdb2d0a522d61cf03
Mahmoud-Al-Jaziri <119842984+Mahmoud-Al-Jaziri@users.noreply.github.com>
2026-05-19 02:27:51 +0800

    send auth token in weather call

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,6 +1,12 @@
+import { auth } from "../firebase";
+
 const API_URL = import.meta.env.VITE_API_URL;
-export async function getTomorrowWeather(lat,lon){
-    const res = await fetch(`${API_URL}/weather?lat=${lat}&lon=${lon}`)
-    if (!res.ok) throw new Error("Failed to fetch weather")
-    return res.json()
+
+export async function getTomorrowWeather(lat, lon) {
+  const token = await auth.currentUser.getIdToken();
+  const res = await fetch(`${API_URL}/weather?lat=${lat}&lon=${lon}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  if (!res.ok) throw new Error("Failed to fetch weather");
+  return res.json();
 }
